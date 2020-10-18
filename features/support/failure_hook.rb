dced5409fb7de34b257e29ca9eb5a9ae28bafdb6
Zaid Ali <zaid.ali@modviron.com>
2020-10-18 23:18:58 +1300

    Report with screenshot added

--- features/support/failure_hook.rb
+++ features/support/failure_hook.rb
@@ -1,17 +1,8 @@
 After do | scenario |
 
     if scenario.failed?
-        #path = "html-report/#{scenario.__id__}.html"
-        #page.driver.browser.save_screenshot(path)
-        #embed(path, "image/png")
-    #end
-
-    #screenshot = "log/screenshots/#{scenario.name}.png"
-    #screenshot = "log/screenshots/apple.png"
-    #page.save_screenshot(screenshot)
-    #puts(screenshot)
-    #embed(screenshot, 'image/png', 'Screen_Evidence')
-    #scenario.attach(screenshot, 'image/png');
-    #scenario.attach('This is some text that also goes into the report');
+        filename = "Failure-#{scenario.name.slice(0..150).gsub(%r(/['\/\000£$]/), '_').gsub(/\s/, '_')}-#{Time.now.to_i}"
+        page.driver.browser.save_screenshot("#{filename}.png")
     end
+
 end
